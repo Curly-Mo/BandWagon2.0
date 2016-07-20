afc723783162895f632d35a29da0c3030175ad57
Colin Fahy <cpfahy@gmail.com>
2016-07-20 16:00:54 -0400

    forgot to commit views

---
+++ views.py
@@ -0,0 +1,17 @@
+import webapp2
+import urllib2
+
+
+class Passthrough(webapp2.RequestHandler):
+    def get(self):
+        url = self.request.get('url', '')
+        ip = self.request.environ.get('HTTP_X_REAL_IP', self.request.remote_addr)
+        url = url.replace('geoip=true', 'geoip={}'.format(ip))
+
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        data = response.read()
+
+        self.response.headers.add_header("Access-Control-Allow-Origin", "*")
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(data)
