d8fe72d1938670f6cf3e714411c16d3c27a0317d
Colin Fahy <cpfahy@gmail.com>
2016-08-18 17:36:51 -0400

    add Array.find polyfill for Android chromium

--- static/js/main.js
+++ static/js/main.js
@@ -2074,3 +2074,28 @@ function facebook_get_music(){
         }
     );
 }
+
+// Array.find polyfill for certain browsers (Android)
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
